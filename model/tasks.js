b5b0b67effd6ce957eec5764379cc69bf2e6a932
)- <a-@-s-MacBook-Pro.local>
2022-06-02 07:10:59 -0400

    model schema set up

---
+++ model/tasks.js
@@ -0,0 +1,17 @@
+//Require Mongoose inorder to create new schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create New Schema
+const TasksSchema = new Schema({
+    task:{
+        type: String,
+        required: true
+    }
+})
+
+const Tasks = mongoose.model('Tasks', TasksSchema);
+
+//export schema so we can use it 
+module.exports = Tasks;
+
