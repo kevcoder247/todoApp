69452b02ae40970af4449deffec2500067c0ffdc
)- <a-@-s-MacBook-Pro.local>
2022-06-02 18:23:52 -0400

    fake post route working

--- model/tasks.js
+++ model/tasks.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 //Create New Schema
 const TasksSchema = new Schema({
-    task:{
+    title:{
         type: String,
         required: true
     }
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+// const { param } = require('express/lib/request');
 const app = express();
 const mongoose = require('mongoose')
+// const methodOverride = require("method-override")
 const todoTasks = require('./model/tasks')
 const PORT = 3000;
 
+//Test database=============================
+const tasks = [ { _id:0, title: 'Go to the gym'}];
+
+let count = 0;
+
+
 // Database configuration
-const DATABASE_URL = 'mongodb+srv://admin:abc321@todoapp.21go0yr.mongodb.net/?retryWrites=true&w=majority'
+const DATABASE_URL = 'mongodb+srv://admin:abc321@todoapp.21go0yr.mongodb.net/todoApp?retryWrites=true&w=majority'
 const db = mongoose.connection;
 
 // Connect to MongoDB Atlas
@@ -19,14 +27,45 @@ db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
+//JSON MIDDLEWARE==============================
+app.use(express.json())
+//MIDDLEWARE===================================
+app.use(express.urlencoded({ extended: false }))
 
 
-//Routes
+//Mount Routes==============================
+//INDEX (list everything)
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', {tasks})
+    // res.send({
+    //     msg: 'Hello',
+    //     user: {}
+    // })
 })
 
+//DESTROY================================
+app.delete('/', (req, res) => {
+    //Add a method that removes the item from the database
+    res.render('index.ejs', {tasks})
+})
 
+//Create========================
+app.post('/', (req, res) => {
+    const task = req.body;
+    tasks.push({...task, _id: ++count})
+    console.log(tasks);
+    res.render('index.ejs', {tasks})
+})
+
+//Update====================================
+app.get('/edit/:id', (req, res) => {
+    res.render('edit.ejs', {task: tasks[req.params.id]});
+})
+
+//SHOW=====================================
+app.get('/show/:id', (req, res) => {
+    res.render('show.ejs', {task: tasks[req.params.id]})
+})
 
 //Make sure we are connected to the server
 app.listen(PORT, () => {
