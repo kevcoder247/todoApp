cd698e37b4c2e0c5a72ceb496b023b4fba5cf109
)- <a-@-s-MacBook-Pro.local>
2022-06-02 07:24:49 -0400

    mongoose database connedted

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const mongoose = require('mongoose')
 const todoTasks = require('./model/tasks')
 const PORT = 3000;
 
+// Database configuration
+const DATABASE_URL = 'mongodb+srv://admin:abc321@todoapp.21go0yr.mongodb.net/?retryWrites=true&w=majority'
+const db = mongoose.connection;
+
+// Connect to MongoDB Atlas
+mongoose.connect(DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+// Database Connection Error/Success
+// Define callback functions for various events
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
 
 
 //Routes
