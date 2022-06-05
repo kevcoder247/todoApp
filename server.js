8522106bfa680503ab1a2ba4b8c6d92295e640fc
)- <a-@-s-MacBook-Pro.local>
2022-06-05 18:26:51 -0400

    edit button added

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ require('dotenv').config();
 const app = express();
 const mongoose = require('mongoose')
 // const methodOverride = require("method-override")
-const todoTasks = require('./model/tasks')
+const Tasks = require('./model/tasks')
 const PORT = 3000;
 
 //Link CSS========================================
 app.use(express.static(__dirname + '/public'));
 
-//Test database=============================
-const tasks = [ { _id:0, title: 'Go to the gym'}];
+
+
+//Test database================================================
+// const tasks = [ { _id:0, title: 'Go to the gym'}];
 //track number of arrays in our test database
-let count = 0;
+// let count = 0;
 
 
 // Database configuration
@@ -39,48 +41,63 @@ app.use(express.urlencoded({ extended: false }))
 
 
 //Mount Routes==============================
-//INDEX (list everything)
-app.get('/', (req, res) => {
-    res.render('index.ejs', {tasks})
-})
+
+//INDEX==================================================>
+app.get('/tasks', (req, res) => {
+    Tasks.find({}, (error, allTasks) => {
+        res.render('index.ejs', {
+            tasks: allTasks,
+        });
+    });
+});
 
 //DESTROY================================
-app.post('/delete/:id', (req, res) => {
-    //Add a method that removes the item from the database
-    tasks.splice(req.params.id, 1)
-    console.log(tasks)
-    if(tasks.length < 1){
-        count = 0
-    }
-    res.redirect('/')
-})
+// app.post('/delete/:id', (req, res) => {
+//     //Add a method that removes the item from the database
+//     tasks.splice(req.params.id, 1)
+//     console.log(tasks)
+//     if(tasks.length < 1){
+//         count = 0
+//     }
+//     res.redirect('/')
+// })
 
-//Create========================
-app.post('/', (req, res) => {
-    const task = req.body;
-    tasks.push({...task, _id: ++count})
-    console.log(tasks);
-    res.redirect('/')
+//Create===================================
+app.post('/tasks', (req, res) => {
+    Tasks.create(req.body, (error, createdTask) => {
+        res.redirect('/tasks')
+    })
  })
+
+// SAVE===============================
+app.post('/edit/:id/save', (req, res) => {
+    Tasks.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+          new: true,
+        },
+        (error, updatedTask) => {
+          res.redirect('/tasks')
+        }
+      )
+    })
 
 //Update====================================
 app.get('/edit/:id', (req, res) => {
-    res.render('edit.ejs', {task: tasks[req.params.id]});
-})
-
-//SAVE===============================
-app.post('/edit/:id/save', (req, res) => {
-    const task = req.body;
-    console.log(task)
-    tasks[task._id] = task;
-    res.redirect('/')//figure out how to update the array with the given body
-    
+    Tasks.findById(req.params.id,
+        (error, task) => {
+            res.render(`edit.ejs`, {
+                task
+            })
+        }
+    )
 })
 
 //SHOW=====================================
-app.get('/show/:id', (req, res) => {
-    res.render('show.ejs', {task: tasks[req.params.id]})
-})
+// app.get('/show/:id', (req, res) => {
+//     res.render('show.ejs', {task: tasks[req.params.id]})
+// })
 
 //Make sure we are connected to the server
 app.listen(PORT, () => {
